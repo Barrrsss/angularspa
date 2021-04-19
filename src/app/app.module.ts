ee257d0393a8cdb3790f0a01880c32b0f2e3a511
Alexey Krasnov <neky@list.ru>
2021-04-19 16:40:34 +0500

    tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskoneComponent } from './taskone/taskone.component';
+import { TasktwoComponent } from './tasktwo/tasktwo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TasksComponent,
+    TaskoneComponent,
+    TasktwoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tasktwo/tasktwo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasktwo',
+  templateUrl: './tasktwo.component.html',
+  styleUrls: ['./tasktwo.component.scss']
+})
+export class TasktwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
