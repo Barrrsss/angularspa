16b1fa99be94268c0368c967bdb621e3ae26ba41
Alexey Krasnov <neky@list.ru>
2021-04-20 16:10:58 +0500

    task two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskoneComponent } from './taskone/taskone.component';
 import { TasktwoComponent } from './tasktwo/tasktwo.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,23 @@ import { TasktwoComponent } from './tasktwo/tasktwo.component';
     TasktwoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: '#78C000',
+      innerStrokeColor: '#C7E596',
+      animationDuration: 300,
+      title: '',
+      subtitle: '',
+      space: -6,
+      showBackground: true,
+      backgroundStroke: '',
+      backgroundPadding: -3,
+      backgroundStrokeWidth: 5,
+      showUnits: false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
